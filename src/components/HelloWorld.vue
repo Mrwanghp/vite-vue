9759c5f26b1f0be9df291580586ffc3fb21c7fcf
wdssss <1595678040@qq.com>
2020-07-16 14:01:00 +0800

    项目构建

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>11112222222211111111111111asdadasdasa1fasf51afs1sa</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  mounted () {
+    console.log(process.env)
+  }
+}
+</script>
